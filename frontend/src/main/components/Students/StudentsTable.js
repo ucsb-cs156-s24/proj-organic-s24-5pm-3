87323775aa95149d6ffdc9901914b00636597c95
Aaron Rosales <arosales@ucsb.edu>
2024-05-29 14:27:21 -0700

    deleted a unused button in Table, and edited some name mistakes in Table.Stories

--- frontend/src/main/components/Students/StudentsTable.js
+++ frontend/src/main/components/Students/StudentsTable.js
@@ -1,9 +1,4 @@
 import React from "react";
- import OurTable, { ButtonColumn } from "main/components/OurTable"
-//  import { useBackendMutation } from "main/utils/useBackend";
-//  import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/components/Utils/SchoolUtils"
-//  import { useNavigate } from "react-router-dom";
- //import { hasRole } from "main/utils/currentUser";
 
  export default function  StudentsTable({ students }) {
      const columns = [
--- frontend/src/stories/components/Students/StudentsTable.stories.js
+++ frontend/src/stories/components/Students/StudentsTable.stories.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import studentsTable from 'main/components/Students/StudentsTable';
-import { studentsFixture } from 'fixtures/studentsFixtures';
+import StudentsTable from 'main/components/Students/StudentsTable';
+import { studentsFixtures } from 'fixtures/studentsFixtures';
 import { currentUserFixtures } from 'fixtures/currentUserFixtures';
 import { rest } from "msw";
 
 export default {
     title: 'components/Students/StudentsTable',
-    component: studentsTable
+    component: StudentsTable
 };
 
 const Template = (args) => {
     return (
-        <studentsTable {...args} />
+        <StudentsTable {...args} />
     )
 };
 
@@ -24,13 +24,13 @@ Empty.args = {
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.args = {
-    students: studentsFixture.threeStudents,
+    students: studentsFixtures.threeStudents,
     currentUser: currentUserFixtures.userOnly,
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 ThreeItemsAdminUser.args = {
-    students: studentsFixture.threeStudents,
+    students: studentsFixtures.threeStudents,
     currentUser: currentUserFixtures.adminUser,
 }
 
