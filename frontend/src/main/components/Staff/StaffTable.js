773ea482030db32a97dcb63e03f03eb74868c72f
sherpatran <tylertran@ucsb.edu>
2024-05-30 18:04:37 -0700

    added the delete feautre to the staff table

--- frontend/src/main/components/Staff/StaffTable.js
+++ frontend/src/main/components/Staff/StaffTable.js
@@ -1,10 +1,22 @@
 import React from "react";
-import OurTable from "main/components/OurTable"
+ import OurTable, { ButtonColumn } from "main/components/OurTable"
+ import { useBackendMutation } from "main/utils/useBackend";
+ import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/components/Utils/StaffUtils"
+ import { hasRole } from "main/utils/currentUser";
 
+ export default function StaffTable({ staff, currentUser }) {
 
- export default function StaffTable({ staff }) {
+     // Stryker disable all : hard to test for query caching
+
+     const deleteMutation = useBackendMutation(
+         cellToAxiosParamsDelete,
+         { onSuccess: onDeleteSuccess },
+         ["/api/courses/all"]
+     );
+     // Stryker restore all 
 
      // Stryker disable next-line all : TODO try to make a good test for this
+     const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
      const columns = [
          {
@@ -19,11 +31,18 @@ import OurTable from "main/components/OurTable"
              Header: 'githubId',
              accessor: 'githubId',
          },
-   
+
+  
      ];
 
-     return <OurTable
-         data={staff}
-         columns={columns}
-         testid={"StaffTable"} />;
+     if (hasRole(currentUser, "ROLE_ADMIN") || hasRole(currentUser, "ROLE_INSTRUCTOR")) {
+         columns.push(ButtonColumn("Delete", "danger", deleteCallback, "StaffTable"));
+     }
+
+
+    
+
+     return (
+          <OurTable data={staff} columns={columns} testid={"StaffTable"} />
+      );
     };
--- frontend/src/main/pages/CoursesStaffPage.js
+++ frontend/src/main/pages/CoursesStaffPage.js
@@ -1,15 +1,43 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from 'react-router-dom'
+import { useBackend } from "main/utils/useBackend";
+import StaffTable from "main/components/Staff/StaffTable";
 
 
 export default function CoursesStaffPage() {
+  let { id } = useParams();
 
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Staff</h1>
-        
-          
-      </div>
-    </BasicLayout>
-  )
+  const { data: staff, _error, status } =
+      useBackend(
+          [`/api/courses/getStaff?courseId=${id}`],
+          {
+              method: "GET",
+              url: `/api/courses/getStaff`,
+              params: {
+                  courseId: id
+              }
+          }
+      );
+
+      if (status === 'loading') {
+        return (
+          <BasicLayout>
+            <div className="pt-2">
+              <h1>Loading...</h1>
+            </div>
+          </BasicLayout>
+        );
+      }
+    
+
+      return (
+        <BasicLayout>
+          <div className="pt-2">
+            <h1>Staff</h1>
+            {
+              <StaffTable staff={staff}/>
+            }
+          </div>
+        </BasicLayout>
+      )
 }
